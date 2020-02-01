fda51789f0e29fd450df74fbbfd28f5f9d9830e0
Kavan Bahrami <kavanbahrami@gmail.com>
2020-02-01 10:44:26 -0800

    Ship Stuff

--- Assets/kavan/Scripts/FloodController.cs
+++ Assets/kavan/Scripts/FloodController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class FloodController : MonoBehaviour
 {
     public Slider m_WaterSlider;
@@ -12,6 +13,12 @@ public class FloodController : MonoBehaviour
     [Range(0,1)]
     public float m_PercentFlooded;
 
+    public bool m_RoomFlooding = false;
+    public bool m_RoomFlooded = false;
+
+    public float m_TimeToFlood = 10f;
+    private float m_timer;
+
     private void Awake()
     {
         defaultColor = m_RoomOverlay.color;
@@ -19,9 +26,21 @@ public class FloodController : MonoBehaviour
 
     private void Update()
     {
-        //Water Level
-        m_WaterSlider.value = m_PercentFlooded;
-        //Color Overlay for lack of Oxygen
-        m_RoomOverlay.color = new Color(defaultColor.r, defaultColor.g, defaultColor.b, m_PercentFlooded);
+        if (m_RoomFlooding && !m_RoomFlooded)
+        {
+            m_timer += Time.deltaTime;
+
+            m_PercentFlooded = m_timer / m_TimeToFlood;
+
+            //Water Level
+            m_WaterSlider.value = m_PercentFlooded;
+            //Color Overlay for lack of Oxygen
+            m_RoomOverlay.color = new Color(defaultColor.r, defaultColor.g, defaultColor.b, m_PercentFlooded);
+
+            if (m_PercentFlooded > .99)
+            {
+                m_RoomFlooded = true;
+            }
+        }
     }
 }
