6f39186c2ec2717fbf77e349e60d23916067d2df
Kavan Bahrami <kavanbahrami@gmail.com>
2020-02-01 11:00:44 -0800

    TMP and Scriptable Objects

--- Assets/kavan/Scripts/ShipController.cs
+++ Assets/kavan/Scripts/ShipController.cs
@@ -7,17 +7,17 @@ public class ShipController : MonoBehaviour
     public GameObject[] Rooms;
 
     public float m_GameTime = 90f;
-    public float m_Timer;
+    public FloatVariable m_Timer;
     public float m_PercentDone;
 
     private void Update()
     {
-        m_Timer += Time.deltaTime;
+        m_Timer.currentValue += Time.deltaTime;
 
-        m_PercentDone = m_Timer / m_GameTime;
+        m_PercentDone = m_Timer.currentValue / m_GameTime;
 
 
-        if (m_Timer >= m_GameTime)
+        if (m_Timer.currentValue >= m_GameTime)
         {
             Debug.Log("GAME OVER, time ran out");
         }
