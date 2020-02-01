64de8364444102b7395174d4c716f0fe8197e54f
Kavan Bahrami <kavanbahrami@gmail.com>
2020-02-01 12:51:57 -0800

    room stuff

--- Assets/kavan/Scripts/ShipController.cs
+++ Assets/kavan/Scripts/ShipController.cs
@@ -38,7 +38,17 @@ public class ShipController : MonoBehaviour
     void Leak()
     {
         var num = Random.Range(0, Rooms.Length);
+        var isCurRoomFlooding = Rooms[num].GetComponent<RoomController>().m_RoomFlooding;
+
+        if (isCurRoomFlooding == true)
+        {
+            Debug.Log("the current room should be flooding already : " + isCurRoomFlooding);
+            return;
+            //jump out and auto try again since Create leak is still true
+        }
+
         Rooms[num].GetComponent<RoomController>().m_RoomFlooding = true;
+
         m_CreateLeak = false;
     }
 }
