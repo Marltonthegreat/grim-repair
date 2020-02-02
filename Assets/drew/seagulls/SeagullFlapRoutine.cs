6b583e7c8ff5fcbaed90b02c456748b3b26a0632
Drew Gingerich <gingerich93@gmail.com>
2020-02-02 10:16:44 -0800

    Add script to make seagulls flap randomly

---
+++ Assets/drew/seagulls/SeagullFlapRoutine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeagullFlapRoutine : MonoBehaviour
+{
+    public Animator anim;
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return null;
+            var waitTime = Random.Range(1f, 5f);
+            yield return new WaitForSeconds(waitTime);
+            anim.SetTrigger("flap");
+        }
+        
+    }
+}
