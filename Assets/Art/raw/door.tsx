cab267aa61bb54b85a4aedcee8a471cd56c37352
Ryan Kubik <rmkubik@me.com>
2020-02-02 09:28:31 -0800

    Fix door resize

--- Assets/Art/raw/door.tsx
+++ Assets/Art/raw/door.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="door" tilewidth="8" tileheight="24" tilecount="3" columns="3">
- <image source="../export/door.png" width="24" height="24"/>
+<tileset version="1.2" tiledversion="1.3.2" name="door" tilewidth="8" tileheight="24" tilecount="5" columns="2">
+ <grid orientation="orthogonal" width="4" height="24"/>
+ <image source="../export/door.png" width="20" height="24"/>
 </tileset>
