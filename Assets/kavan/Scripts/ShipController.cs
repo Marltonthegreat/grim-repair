09fa446998c0deecc7d5475f3929b9c46d0ffb09
Kavan Bahrami <kavanbahrami@gmail.com>
2020-02-02 11:44:08 -0800

    Draining works ???

--- Assets/kavan/Scripts/ShipController.cs
+++ Assets/kavan/Scripts/ShipController.cs
@@ -6,28 +6,28 @@ public class ShipController : MonoBehaviour
 {
     public GameObject[] Rooms;
 
-    public float m_GameTime = 90f;
-    public FloatVariable m_TimeRemaining;
-    public FloatVariable m_AirRemaining;
+    //public float m_GameTime = 90f;
+    //public FloatVariable m_TimeRemaining;
+    //public FloatVariable m_AirRemaining;
     public bool m_CreateLeak = false;
 
-    private void Awake()
-    {
-        m_TimeRemaining.currentValue = m_GameTime;
-    }
+   // private void Awake()
+  //  {
+        //m_TimeRemaining.currentValue = m_GameTime;
+  //  }
 
     private void Update()
     {
         //Update Time Remaining
-        m_TimeRemaining.currentValue -= Time.deltaTime;
+        //m_TimeRemaining.currentValue -= Time.deltaTime;
 
         //Update Air Remaining (if it should be a % of time remaining)
-        m_AirRemaining.currentValue = m_TimeRemaining.currentValue / m_GameTime;
+        //m_AirRemaining.currentValue = m_TimeRemaining.currentValue / m_GameTime;
 
-        if (m_TimeRemaining.currentValue <= 0)
-        {
-            Debug.Log("GAME OVER, time ran out");
-        }
+       // if (m_TimeRemaining.currentValue <= 0)
+        //{
+         //   Debug.Log("GAME OVER, time ran out");
+        //}
 
         if (m_CreateLeak)
         {
