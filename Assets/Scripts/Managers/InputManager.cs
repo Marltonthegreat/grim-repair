31bddf4a51597a05f4558aa8fdf13bc9d3abc3b5
carlosbarajas19 <cbarajas@student.neumont.edu>
2023-01-26 08:49:51 -0700

    1/26/23 Finished keyboard rework

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -9,10 +9,36 @@ public class InputManager : MonoBehaviour
     [System.NonSerialized]
     public List<Player> players = new List<Player>();
 
+    PlayerInput wasdPlayer = null;
+    PlayerInput arrowPlayer = null;
+
+    private void Update()
+    {
+        if (!arrowPlayer && Input.GetKeyDown(KeyCode.DownArrow) && wasdPlayer != null)
+        {
+            wasdPlayer.SwitchCurrentControlScheme("KeyboardLeft", Mouse.current);
+        }
+    }
+
     void OnPlayerJoined(PlayerInput playerInput) {
         Debug.Log($"Player joined: {playerInput.name}, {playerInput.devices.Count}");
+        Debug.Log(playerInput.currentControlScheme);
         var player = playerInput.GetComponent<Player>();
         players.Add(player);
+
+        if (playerInput.currentControlScheme == "Keyboard&Mouse" && wasdPlayer == null)
+        {
+            wasdPlayer = playerInput;
+            playerInput.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
+        }
+        else if((playerInput.currentControlScheme == "Keyboard&Mouse" && wasdPlayer != null)||playerInput.currentControlScheme == null)
+        {
+            arrowPlayer = playerInput;
+            playerInput.SwitchCurrentControlScheme("KeyboardRight", Keyboard.current);
+            wasdPlayer.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
+            Debug.Log(wasdPlayer.currentControlScheme);
+        }
+
         player.SetColor(playerColors[players.Count - 1]);
     }
 
