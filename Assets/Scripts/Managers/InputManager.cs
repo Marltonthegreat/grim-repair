8ae332c3b2ff6dc0a40ffd5528682ffd29456d30
carlosbarajas19 <cbarajas@student.neumont.edu>
2023-01-26 10:22:41 -0700

    1/26/23 Fixed keyboard rework

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -12,11 +12,31 @@ public class InputManager : MonoBehaviour
     PlayerInput wasdPlayer = null;
     PlayerInput arrowPlayer = null;
 
-    private void Update()
+    bool arrowPlayerJoined;
+    bool wasdPlayerJoined;
+
+    
+
+    private void FixedUpdate()
     {
-        if (!arrowPlayer && Input.GetKeyDown(KeyCode.DownArrow) && wasdPlayer != null)
+        if (!arrowPlayerJoined && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) )
         {
-            wasdPlayer.SwitchCurrentControlScheme("KeyboardLeft", Mouse.current);
+            wasdPlayer?.SwitchCurrentControlScheme("KeyboardLeft", Mouse.current);
+            arrowPlayerJoined = true;
+            /*if (arrowPlayer)
+            {
+                arrowPlayer.SwitchCurrentControlScheme("KeyboardRight", Keyboard.current);  
+            }*/
+        }
+
+        if (!wasdPlayerJoined && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)))
+        {
+            arrowPlayer?.SwitchCurrentControlScheme("KeyboardRight", Mouse.current);
+            wasdPlayerJoined = true;
+            /*if (wasdPlayer)
+            {
+                wasdPlayer.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
+            }*/
         }
     }
 
@@ -26,17 +46,24 @@ public class InputManager : MonoBehaviour
         var player = playerInput.GetComponent<Player>();
         players.Add(player);
 
-        if (playerInput.currentControlScheme == "Keyboard&Mouse" && wasdPlayer == null)
+        if (playerInput.currentControlScheme == "Keyboard&Mouse" || playerInput.currentControlScheme == null)
         {
-            wasdPlayer = playerInput;
-            playerInput.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
-        }
-        else if((playerInput.currentControlScheme == "Keyboard&Mouse" && wasdPlayer != null)||playerInput.currentControlScheme == null)
-        {
-            arrowPlayer = playerInput;
-            playerInput.SwitchCurrentControlScheme("KeyboardRight", Keyboard.current);
-            wasdPlayer.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
-            Debug.Log(wasdPlayer.currentControlScheme);
+            if (wasdPlayerJoined && !wasdPlayer)
+            {
+                wasdPlayer = playerInput;
+                playerInput.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
+
+                arrowPlayer?.SwitchCurrentControlScheme("KeyboardRight", Keyboard.current);
+
+            }
+            else if (arrowPlayerJoined && !arrowPlayer)
+            {
+                arrowPlayer = playerInput;
+                playerInput.SwitchCurrentControlScheme("KeyboardRight", Keyboard.current);
+
+                wasdPlayer?.SwitchCurrentControlScheme("KeyboardLeft", Keyboard.current);
+
+            }
         }
 
         player.SetColor(playerColors[players.Count - 1]);
